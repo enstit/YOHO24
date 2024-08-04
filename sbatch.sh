aa5b18849286c863ef92e90cfe2a49bce3c91d19
Davide Capone <davidecapone2304@gmail.com>
2024-08-04 21:24:02 +0200

    Update sbatch file

--- sbatch.sh
+++ sbatch.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=yoho
-#SBATCH --account=dcapone
+#SBATCH -p GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpu-per-task=12
+#SBATCH --gres=gpu:2
+#SBATCH --time=02:00:00
 #SBATCH --output=my_job_%j.out
+#SBATCH --job-name=yoho24
 
 # Activate the virtual environment
 source $(pwd)/yoho24/bin/activate
