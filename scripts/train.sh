53ca9d32d19a4c22ef0f52d6019ab33bd99d18f5
Enrico Stefanel <estefanel@gpu001.hpc.rd.areasciencepark.it>
2024-09-08 10:20:21 +0200

    feat: Update YOHO24 trainig results

---
+++ scripts/train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -p GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+# #SBATCH --cpu-per-task=12
+#SBATCH --gres=gpu:2
+#SBATCH --time=02:00:00
+#SBATCH --output=_%j.out
+#SBATCH --job-name=yoho24
+
+# Activate the virtual environment
+source /u/dssc/$(whoami)/scratch/torchenv/bin/activate
+
+cd /u/dssc/$(whoami)/scratch/YOHO24/
+
+# Run the train.py script
+python3 -m yoho.train
+
+# Commit the changes to the repository
+git add .
+git commit -m "feat: Update YOHO24 trainig results"
+git push
---
+++ scripts/venv.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -p GPU
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+# #SBATCH --cpu-per-task=12
+#SBATCH --gres=gpu:2
+#SBATCH --time=02:00:00
+#SBATCH --output=_%j.out
+#SBATCH --job-name=yoho24
+
+# Create a new virtual environment with Python 3.11.0 and install the needed packages
+
+cd /u/dssc/$(whoami)/scratch/
+
+wget https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tgz
+tar -xzf Python-3.11.0.tgz
+rm Python-3.11.0.tgz
+cd Python-3.11.0/
+./configure --enable-optimizations CC="gcc -pthread" CXX="g++ -pthread" --enable-loadable-sqlite-extensions
+make -j 24
+
+cd /u/dssc/$(whoami)/scratch
+
+python3 -m virtualenv --python="Python-3.11.0/python" torchenv3.11
+
+# Activate the virtual environment
+source /u/dssc/$(whoami)/scratch/torchenv3.11/bin/activate
+
+cd /u/dssc/$(whoami)/scratch/YOHO24/
+
+# Upgrade Pip to the latest version
+pip install --upgrade pip
+
+# Install the needed packages
+pip install -r requirements.txt
