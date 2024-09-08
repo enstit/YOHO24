06fd85085d6402ad5ab595de683bd79d94596da0
Enrico Stefanel <me@enst.it>
2024-09-08 18:40:44 +0200

    feat: Run the Jupyter notebook and commit on training end

--- scripts/train.sh
+++ scripts/train.sh
@@ -16,7 +16,9 @@ cd /u/dssc/$(whoami)/scratch/YOHO24/
 # Run the train.py script
 python3 -m yoho.train
 
+jupyter nbconvert --to notebook --execute notebooks/02_urbansed.ipynb
+
 # Commit the changes to the repository
-#git add .
-#git commit -m "feat: Update YOHO24 trainig results"
-#git push
+git add .
+git commit -m "feat: Update YOHO24 trainig results"
+git push
